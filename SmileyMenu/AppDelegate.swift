c8e1de4e3122224349f41e7f22c4ddb15f1091f2
Bart van Kuik <bart@vankuik.nl>
2018-08-10 10:36:17 +0100

    Initial Commit

---
+++ SmileyMenu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SmileyMenu
+//
+//  Created by Bart van Kuik on 10/08/2018.
+//  Copyright © 2018 DutchVirtual. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
